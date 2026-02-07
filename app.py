8db9407f1fdbbab25ed6a5d0d0c092f12943782c
JadTirani <jadtirani4@gmail.com>
2026-02-06 21:17:38 -0500

    Entry fixed

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask
 class app :
     def __init__(self) :
         self.flask = Flask(__name__)
+        self.flask.add_url_rule("/", view_func=self.Index)
 
     def Run(self) :
-        self.flask.run()
+        self.flask.run(debug=True, host="0.0.0.0", port=5000)
 
     def Destroy(self) :
         pass
+
+    def Index(self) :
+        return "<h1>Home Page</h1>"
---
+++ requests.py
@@ -0,0 +1,5 @@
+from enum import StrEnum
+
+class REQUEST(StrEnum) :
+    GET="GET"
+    

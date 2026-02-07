bab9c1cef2fc3e6fa2af28bf9b410544e18bcaf7
JadTirani <jadtirani4@gmail.com>
2026-02-06 22:32:51 -0500

    fixed our snack commit from before

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from templates.countdown.countdown import countdown_index
 
+from flask import redirect, url_for
 class app :
     def __init__(self) :
         self.flask = Flask(__name__)
         self.flask.add_url_rule("/", view_func=self.Index)
+        self.flask.add_url_rule("/CD", endpoint="CD", view_func=countdown_index)
 
     def Run(self) :
         self.flask.run(debug=True, host="0.0.0.0", port=5000)
@@ -12,4 +15,4 @@ class app :
         pass
 
     def Index(self) :
-        return "<h1>Home Page</h1>"
+        return redirect(url_for("CD"))
--- requests.py
+++
@@ -1,5 +0,0 @@
-from enum import StrEnum
-
-class REQUEST(StrEnum) :
-    GET="GET"
-    
